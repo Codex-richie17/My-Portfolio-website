221489a14a0cf3bb5121e4fa80d3d31b2f9ed625
Codex-richie17 <macricch1047@gmail.com>
2025-01-14 20:05:48 -0700

    Add files via upload

---
+++ My Portfolio/My-Portfolio-website/My Portfolio/My-Portfolio-website/script.js
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+=======
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
+}
+>>>>>>> 71958f395fdac87afa2cf07becccd5d4a5dc992b
